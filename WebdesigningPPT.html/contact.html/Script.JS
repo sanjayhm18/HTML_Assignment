e06cc737d912f2f260871ee1186a4e427ca61d08
sanjayhm18 <sanjayhm6698@gmail.com>
2024-08-04 13:46:47 +0530

    WEB PPT
    WEB DESIGNING PPT

---
+++ WebdesigningPPT.html/contact.html/Script.JS
@@ -0,0 +1,9 @@
+// JavaScript can be used for interactivity if needed
+document.addEventListener('DOMContentLoaded', () => {
+    // Example: Add any interactive elements or features here
+    function changeimage()
+  {
+    var img=document.getElementById('image');
+    img.src='https://th.bing.com/th/id/OIP.f2U4Twyypuz0DxRNs9TW7QHaE8?w=232&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7';
+  }
+  });
